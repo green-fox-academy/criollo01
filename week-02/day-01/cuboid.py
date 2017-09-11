c4010b8d68d40ec5c9d78469f4f6177402064441
Criollo01 <lucanagy@protonmail.com>
2017-09-11 15:11:08 +0200

    Finished cuboid.py

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,13 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+
+length = 20
+width = 10
+height = 15
+
+surface_area = 2 * (length * width + width * height + length * height)
+volume = length * width * height
+
+print "Surface Area:", surface_area
+print "Volume:", volume
