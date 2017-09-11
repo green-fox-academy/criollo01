ff29ebdb5e6b561dd0217ba03603e3a710220158
Criollo01 <lucanagy@protonmail.com>
2017-09-11 15:58:18 +0200

    Finished animals_and_legs.py

--- week-02/day-01/animals_and_legs.py
+++ week-02/day-01/animals_and_legs.py
@@ -2,3 +2,8 @@
 # The first represents the number of chickens the farmer has
 # The seconf represents the number of pigs the farmer has
 # It should print how many legs all the animals have
+
+chickens = int(raw_input("How many chickens does the farmer have? "))
+pigs = int(raw_input("How many pigs does the farmer have? "))
+
+print((chickens * 2) + (pigs * 4))
