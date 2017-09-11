fd4eeddc99899dffbfd7f050334cea8cf1a48640
Criollo01 <lucanagy@protonmail.com>
2017-09-11 14:16:15 +0200

    Finished favorite_number.py

--- week-02/day-01/favorite_number.py
+++ week-02/day-01/favorite_number.py
@@ -1,5 +1,5 @@
 # Store your favorite number in a variable (as a number)
 # And print it like this: "My favorite number is: 8"
 
-favorite_number = 5
-print "My favourite number is:", favorite_number
+favorite_number = 8
+print "My favorite number is:", favorite_number
