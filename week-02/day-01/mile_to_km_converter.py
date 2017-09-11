1c3810dbe68d39b9ad61393351b34a113d8731ca
Criollo01 <lucanagy@protonmail.com>
2017-09-11 15:51:17 +0200

    It's working but gf verify said it's a fail

--- week-02/day-01/mile_to_km_converter.py
+++ week-02/day-01/mile_to_km_converter.py
@@ -1,2 +1,8 @@
 # Write a program that asks for an integer that is a distance in kilometers,
 # then it converts that value to miles and prints it
+
+km = float(raw_input("Write a distance in kilometers: "))
+conv = 0.621
+miles = (km * conv)
+
+print miles
