63cd83a0b2a9127cc55f2b36bc934f5f2fc6f6b8
Criollo01 <lucanagy@protonmail.com>
2017-09-11 16:36:58 +0200

    Finished print_bigger.py

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,9 @@
 # Write a program that asks for two numbers and prints the bigger one
+
+number1 = input("Write a number! ")
+number2 = input("Write another, and Imma tell you which was the bigger one: ")
+
+if number1 > number2:
+    print(number1)
+else:
+    print(number2)
