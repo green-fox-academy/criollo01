c102fa0b8b409905fa2238de0822ab9af00c0411
Criollo01 <lucanagy@protonmail.com>
2017-09-11 14:04:07 +0200

    Finished coding_hours.py

--- week-02/day-01/coding_hours.py
+++ week-02/day-01/coding_hours.py
@@ -7,3 +7,16 @@
 # Print the percentage of the coding hours in the semester if the average
 # work hours weekly is 52
 
+coding_hours = (6 * (17 * 5))
+print coding_hours
+
+weekly = coding_hours / 17
+print (weekly)
+
+print (float(weekly) / 52 * 100)
+
+
+
+
+
+
