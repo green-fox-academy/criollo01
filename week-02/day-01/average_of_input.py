a00c96fd5307d655480dfcd944b651d6326d9904
Criollo01 <lucanagy@protonmail.com>
2017-09-11 16:08:07 +0200

    Finished average_of_input.py

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,11 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+
+a = int(raw_input("Write a number! "))
+b = int(raw_input("Write one more! "))
+c = int(raw_input("And one more! "))
+d = int(raw_input("And one more! "))
+e = int(raw_input("And one more! "))
+
+print "Sum:", (a+b+c+d+e), ", Average:", (a+b+c+d+e) / 5
