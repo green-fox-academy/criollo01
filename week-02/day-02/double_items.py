9ba21b6232df069241b66e5d4d6964c380f23f89
Criollo01 <lucanagy@protonmail.com>
2017-09-12 13:19:25 +0200

    Finished double_items.py

---
+++ week-02/day-02/double_items.py
@@ -0,0 +1,9 @@
+# - Create an array variable named `ag`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Double all the values in the array
+
+ag = [3, 4, 5, 6, 7]
+
+ag = [x * 2 for x in ag]
+
+print(ag)
