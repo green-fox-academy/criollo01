940a2a28718dc3af196e1072209e09350eb463fa
Criollo01 <lucanagy@protonmail.com>
2017-09-12 15:38:39 +0200

    Finished sum_all.py

---
+++ week-02/day-02/sum_all.py
@@ -0,0 +1,13 @@
+# - Create a variable named `ai`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Print the sum of the elements in `ai`
+
+ai = [3, 4, 5, 6, 7]
+
+def sum_list(ai):
+    sum = 0
+    for i in ai:
+        sum += i
+    return sum
+
+print(sum_list(ai))
