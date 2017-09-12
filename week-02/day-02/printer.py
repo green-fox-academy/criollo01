d9371de20b23294298734521738c081488329104
Criollo01 <lucanagy@protonmail.com>
2017-09-12 11:54:56 +0200

    Finished printer.py

---
+++ week-02/day-02/printer.py
@@ -0,0 +1,9 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer():
+    input_words = input("Write something! ")
+    print(input_words)
+
+printer()
