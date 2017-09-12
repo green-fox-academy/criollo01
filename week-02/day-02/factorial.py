0840d45d0cd0e311cfa1b623c2f2970afb9f9d1c
Criollo01 <lucanagy@protonmail.com>
2017-09-12 11:49:17 +0200

    Finished factorial.py

--- week-02/day-02/factorial.py
+++ week-02/day-02/factorial.py
@@ -1,2 +1,10 @@
 # - Create a function called `factorio`
 #   that returns it's input's factorial
+
+def factorio(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorio(n-1)
+
+print(factorio(5))
