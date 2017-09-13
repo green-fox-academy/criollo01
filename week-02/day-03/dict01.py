f5caabccca97ec6f6a45df2d2656bae82122221e
Criollo01 <lucanagy@protonmail.com>
2017-09-13 16:03:36 +0200

    dict01.py: it's working

--- week-02/day-03/dict01.py
+++ week-02/day-03/dict01.py
@@ -1,5 +1,5 @@
 students = [
-        {'name': 'Teodor', 'age': 3, 'candies': 2}
+        {'name': 'Teodor', 'age': 3, 'candies': 2},
         {'name': 'Rezso', 'age': 9.5, 'candies': 2},
         {'name': 'Zsombor', 'age': 12, 'candies': 5},
         {'name': 'Aurel', 'age': 7, 'candies': 3},
@@ -12,3 +12,18 @@ students = [
 
 # create a function that takes a list of students and prints:
 # - Sum of the age of people who have lass than 5 candies
+
+# def candies_number():
+
+def student_candy_ate():
+        for i in range(len(students)):
+                print(students[i]['name'] + " : " + str(students[i]['candies']) )
+student_candy_ate()
+
+def sum():
+        sum=0
+        for i in range(len(students)):
+                if students[i]['candies']<5:
+                        sum += students[i]['age']
+        print("The sum is: ", sum)
+sum()
