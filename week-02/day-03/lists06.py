6884611698a72da9a54328cd9618c464dcbbbd1e
Criollo01 <lucanagy@protonmail.com>
2017-09-13 13:46:01 +0200

    lists06.py: added task description

---
+++ week-02/day-03/lists06.py
@@ -0,0 +1,6 @@
+# Check if list contains all of the following elements: 4,8,12,16
+# Create a function that accepts list_of_numbers as an input
+# it should return "True" if it contains all, otherwise "False"
+
+list_of_numbers = [2, 4, 6, 8, 10, 12, 14, 16]
+
