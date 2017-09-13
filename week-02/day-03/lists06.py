078f56920db0008ca0cb3de5b2f67e86574226f6
Criollo01 <lucanagy@protonmail.com>
2017-09-13 16:39:57 +0200

    lists06.py: working but I didn't figure it out

--- week-02/day-03/lists06.py
+++ week-02/day-03/lists06.py
@@ -3,4 +3,21 @@
 # it should return "True" if it contains all, otherwise "False"
 
 list_of_numbers = [2, 4, 6, 8, 10, 12, 14, 16]
+need_to_contain = [4, 8, 12, 16]
 
+
+def is_all_element_in_the_list(need_to_contain, list_of_numbers):
+    for element in need_to_contain:
+        if not element in list_of_numbers:
+            return False
+    return True
+
+# vagy:
+# def is_all_element_in_the_list(need_to_contain, list_of_numbers):
+#     is_ok = True
+#     for element in need_to_contain:
+#         if not element in list_of_numbers:
+#             return False
+#     return True
+
+print(is_all_element_in_the_list(need_to_contain, list_of_numbers))
