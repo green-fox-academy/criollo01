9b50e7e3a0e96adffd6ad9e704769b3f9427d6b4
Criollo01 <lucanagy@protonmail.com>
2017-09-14 11:56:50 +0200

    Finished draw_diamond.py

--- week-02/day-01/draw_diamond.py
+++ week-02/day-01/draw_diamond.py
@@ -11,3 +11,8 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+
+number = int(input("Write a number! "))
+
+for i in range(0, number):
+    print(" " * (number - i) + "*" * (i * 2 - 1))
