ad708a57df218a7ee09ae367cc4b0e33ac0049ad
Criollo01 <lucanagy@protonmail.com>
2017-09-14 10:20:43 +0200

    Finished i_wont_cheat_on_the_exams.py

--- week-02/day-01/i_wont_cheat_on_the_exams.py
+++ week-02/day-01/i_wont_cheat_on_the_exams.py
@@ -1,4 +1,11 @@
 # Create a program that writes this line 100 times:
 # "I won't cheat on the exam!"
 
+# First, simpler solution
+
 print("I won't cheat on the exam!" * 100)
+
+# Another solution with "for" loop
+
+for i in range(0, 99):
+    print("I won't cheat on the exams!")
