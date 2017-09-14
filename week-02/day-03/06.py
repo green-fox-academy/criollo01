94895c154eadbd45b000a29e6a9b808f31803c4d
Criollo01 <lucanagy@protonmail.com>
2017-09-14 11:57:18 +0200

    06.py: it's working

--- week-02/day-03/06.py
+++ week-02/day-03/06.py
@@ -7,19 +7,18 @@
 not_so_cryptic_message = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11]
 
 hashmap = {
-    7: "run around and desert you",
-    50: "tell a lie and hurt you",
-    49: "make you cry,",
-    2: "let you down",
-    12: "give you up,",
+    7: " run around and desert you",
+    50: " tell a lie and hurt you",
+    49: " make you cry,",
+    2: " let you down",
+    12: " give you up,",
     1: "Never gonna",
     11: "\n",
-    3: "say goodbye"
+    3: " say goodbye"
 }
-
 out = ""
- 
-#  .join(str(value) for value in hashmap.items());
-# # result = "".join(str(key) + str(value) for key, value in dictionary.items())
+
+for i in not_so_cryptic_message:
+    out = out + hashmap[i]
 
 print(out)
