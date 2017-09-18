247fba0aa7981d7a3c17437a78dabb295609aeae
Criollo01 <lucanagy@protonmail.com>
2017-09-18 12:18:04 +0200

    it's working

--- week-03/day-01/write_single_line.py
+++ week-03/day-01/write_single_line.py
@@ -2,3 +2,13 @@
 # Write your name in it as a single line
 # If the program is unable to write the file,
 # then it should print an error message like: "Unable to write file: my-file.txt"
+
+def file_writer(filename):
+    file_name = "my-file.txt"
+    try:
+        f = open(file_name, "w")
+        f.write("Nagy Luca")
+    except:
+        print("Unable to write file: ", file_name)
+
+file_writer(file_name)
