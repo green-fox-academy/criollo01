1f55f745fc3775b6e519b2ce4c75755e7e2831c2
Criollo01 <lucanagy@protonmail.com>
2017-09-18 11:36:19 +0200

    it's working

---
+++ week-03/day-01/print-each-line.py
@@ -0,0 +1,14 @@
+# Write a program that opens a file called "my-file.txt", then prints
+# each of lines form the file.
+# If the program is unable to read the file (for example it does not exists),
+# then it should print an error message like: "Unable to read file: my-file.txt"
+
+def file_opener():
+    file_name = "my-file.txt"
+    try:
+        opener = open("my-file.txt", "r")
+        opener.readlines()
+    except:
+        print("unable to read file")
+
+file_opener()
