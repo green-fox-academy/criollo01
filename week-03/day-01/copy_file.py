06123a02315f77579244ddafa5aa42aa0988aabe
Criollo01 <lucanagy@protonmail.com>
2017-09-18 16:14:11 +0200

    it's working but no boolean at the end

--- week-03/day-01/copy_file.py
+++ week-03/day-01/copy_file.py
@@ -1,3 +1,21 @@
 # Write a function that copies a file to an other
 # It should take the filenames as parameters
 # It should return a boolean that shows if the copy was successful
+
+
+def copy_file(path_from, path_to):
+    try:
+        file_open = open(path_from, "r")
+        text = file_open.read()
+        file_open.close()
+    except IOError:
+        print("Unable to read file:", path_from)
+    
+    try:
+        file_2_open = open(path_to, "a")
+        file_2_open.write(text)
+        file_2_open.close()
+    except IOError:
+        print("Unable to write file:", path_to)
+    
+copy_file("file-name.txt", "my-file.txt")
