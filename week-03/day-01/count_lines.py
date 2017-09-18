f1dcde0ea0002b09b2992dc6b2916c95e32a67d7
Criollo01 <lucanagy@protonmail.com>
2017-09-18 12:05:37 +0200

    it's working

--- week-03/day-01/count_lines.py
+++ week-03/day-01/count_lines.py
@@ -3,3 +3,14 @@
 # It should return zero if it can't open the file, and
 # should not raise any error.
 
+file_name = input("Add a filename! ")
+
+def number_of_lines(file_name):
+    try:
+        opener = open(file_name, "r")
+        count = sum(1 for line in open(file_name))
+        print(count)
+    except FileNotFoundError:
+        return 0
+
+number_of_lines(file_name)
