32a34c377985e1f0f23fc7be660d6d9716061464
Criollo01 <lucanagy@protonmail.com>
2017-09-18 15:58:04 +0200

    it works

---
+++ week-03/day-01/write_multiple_lines.py
@@ -0,0 +1,22 @@
+# Create a function that takes 3 parameters: a path, a word and a number,
+# than it should write to a file.
+# The path parameter should be a string, that describes the location of the file.
+# The word parameter should be a string, that will be written to the file as lines
+# The number paramter should describe how many lines the file should have.
+# So if the word is "apple" and the number is 5, than it should write 5 lines
+# to the file and each line should be "apple"
+# The function should not raise any error if it could not write the file.
+
+
+
+def write_multiple_line(path, word, number):
+    multiple_line = open(path, "w")
+    try:
+        for i in range(number):
+            multiple_line.write(word + "\n")
+    except:
+        return 0
+
+
+    multiple_line.close()
+write_multiple_line("/home/bella/Dev/GF/criollo01/week-03/day-01/write_multiple_lines.txt", "apple", 5)
