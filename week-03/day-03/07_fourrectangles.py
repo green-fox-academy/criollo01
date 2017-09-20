dfbeda91b642afad179544c48fda6da1c670a00c
Criollo01 <lucanagy@protonmail.com>
2017-09-20 12:10:37 +0200

    refactor(07_fourrectangles): change variable names

--- week-03/day-03/07_fourrectangles.py
+++ week-03/day-03/07_fourrectangles.py
@@ -7,10 +7,10 @@ root = Tk()
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
-square_red = canvas.create_rectangle(0, 0, 55, 55, fill = 'red')
-square_blue = canvas.create_rectangle(40, 60, 130, 155, fill = 'blue')
-square_green = canvas.create_rectangle(140, 30, 155, 155, fill = 'green')
-square_yellow = canvas.create_rectangle(155, 170, 290, 280, fill = 'yellow')
+rectangle_red = canvas.create_rectangle(0, 0, 55, 55, fill = 'red')
+rectangle_blue = canvas.create_rectangle(40, 60, 130, 155, fill = 'blue')
+rectangle_green = canvas.create_rectangle(140, 30, 155, 155, fill = 'green')
+rectangle_yellow = canvas.create_rectangle(155, 170, 290, 280, fill = 'yellow')
 
 
 root.mainloop()
