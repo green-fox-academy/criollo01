d1fb2ab91c7a4155b93802ea53c31905b1dbeaaf
Criollo01 <lucanagy@protonmail.com>
2017-09-20 12:07:07 +0200

    feat(08_positionsquare): it's working

---
+++ week-03/day-03/08_positionsquare.py
@@ -0,0 +1,20 @@
+# create a square drawing function that takes 2 parameters:
+# the x and y coordinates of the square's top left corner
+# and draws a 50x50 square from that point.
+# draw 3 squares with that function.
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def position_square(x, y):
+    square = canvas.create_rectangle(x, y, x+50, y+50, fill = 'green')
+
+position_square(200, 230)
+position_square(30, 130)
+position_square(70, 190)
+
+root.mainloop()
