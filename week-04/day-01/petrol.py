77a9736097cf2f01182305a3b1cd858bf7993b1e
Criollo01 <lucanagy@protonmail.com>
2017-09-25 15:30:44 +0200

    feat(petrol):first try

---
+++ week-04/day-01/petrol.py
@@ -0,0 +1,24 @@
+''' Create Station and Car classes
+- Station
+  - gas_amount
+  - refill(car) -> decreases the gasAmount by the capacity of the car and increases the cars gas_amount
+- Car
+  - gas_amount
+  - capacity
+  - create constructor for Car where:
+    - initialize gas_amount -> 0
+    - initialize capacity -> 100'''
+
+class Station(object):
+    def __init__(self):
+        self.gas_amount = int(100)
+    
+    def refill(car):
+        self.gas_amount -= car.capacity
+        return self.gas_amount
+
+class Car(object):
+    def __init__(self, gas_amount, capacity):
+        self.gas_amount = int(0)
+        self.capacity = int(100)
+    
