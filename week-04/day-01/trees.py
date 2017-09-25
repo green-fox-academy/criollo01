144241634a449cf11bb69bfbc077a614de7c17eb
Criollo01 <lucanagy@protonmail.com>
2017-09-25 07:59:22 +0200

    feat(trees): added trees

---
+++ week-04/day-01/trees.py
@@ -0,0 +1,16 @@
+# Create 5 trees
+# Store the data of them in variables in your program
+# for every tree the program should store its'
+# type
+# leaf color
+# age
+# sex
+# you can use just variables, or lists and/or maps
+
+trees = [
+    {'tree_type' : 'black birch', 'leaf_color' : 'green', 'age' : 40, 'sex': 'f'},
+    {'tree_type' : 'red pine', 'leaf_color' : 'green', 'age' : 15, 'sex': 'm'},
+    {'tree_type' : 'walnut', 'leaf_color' : 'green', 'age' : 130, 'sex': 'm'},
+    {'tree_type' : 'black oak', 'leaf_color' : 'green', 'age' : 76, 'sex': 'm'},
+    {'tree_type' : 'chestnut', 'leaf_color' : 'green', 'age' : 2, 'sex': 'f'}
+]
