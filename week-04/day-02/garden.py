9ba796faeaaa4760445063c897f9cc2e4913a1d1
Criollo01 <lucanagy@protonmail.com>
2017-09-26 16:12:41 +0200

    feat(garden): it's working

--- week-04/day-02/garden.py
+++ week-04/day-02/garden.py
@@ -1,4 +1,5 @@
 class Garden(object):
+
     def __init__(self):
         self.plants = []
 
@@ -13,5 +14,58 @@ class Garden(object):
                 self.watering.append(plant)
         for plant in self.watering:
             plant.watered(amount/len(self.watering))
-        print(self)
+        return(self)
 
+    def __repr__(self):
+        all_plants = ""
+        for i in range(len(self.plants)):
+             all_plants += str(self.plants[i]) + "\n"
+        return all_plants
+
+class Tree(object):
+
+    def __init__(self, color):
+        self.color = color
+        self.water_amount = 0
+
+    def needs_water(self):
+        if self.water_amount < 10:
+            return "needs water"
+        else:
+            return "doesn't need water"
+
+    def watered(self, amount):
+        self.water_amount += amount * 0.4
+    
+    def __repr__(self):
+        return "The {} tree {}.".format(self.color, self.needs_water())
+
+class Flower(Tree):
+
+    def needs_water(self):
+        if self.water_amount < 5:
+            return "needs water"
+        else:
+            return "doesn't need water"
+
+    def watered(self, amount):
+        self.water_amount += amount * 0.75
+
+    def __repr__(self):
+        return "The {} flower {}.".format(self.color, self.needs_water())
+
+the_garden = Garden()
+the_garden.add(Tree("brown"))
+the_garden.add(Tree("green"))
+the_garden.add(Flower("red"))
+the_garden.add(Flower("yellow"))
+
+print(the_garden)
+
+the_garden.water(40)
+
+print(the_garden)
+
+the_garden.water(70)
+
+print(the_garden)
