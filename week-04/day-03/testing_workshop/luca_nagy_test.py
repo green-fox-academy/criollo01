6c04b1e99a85303b5221f7c0f0460b27d1a1c8bd
Criollo01 <lucanagy@protonmail.com>
2017-09-27 12:14:23 +0200

    refact(luca_nagy_test): change file name

---
+++ week-04/day-03/testing_workshop/luca_nagy_test.py
@@ -0,0 +1,11 @@
+import unittest
+from apple import Apple
+
+class TestApple(unittest.TestCase):
+    
+    def test_get_apple(self):
+        test_apple = Apple()
+        self.assertEqual(test_apple.get_apple(), "apple")
+
+if __name__ == '__main__':
+    unittest.main()
