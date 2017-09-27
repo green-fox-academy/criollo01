00b0fdca07b758ce239f5245a4515066b5ff5f8c
Criollo01 <lucanagy@protonmail.com>
2017-09-27 16:16:48 +0200

    feat(luca_nagy_test): instead of assert equals, use assert equal

--- week-04/day-03/luca_nagy_test.py
+++ week-04/day-03/luca_nagy_test.py
@@ -1,5 +1,5 @@
 import unittest
-from luca_nagy_work import Apple, Sum, Anagram
+from luca_nagy_work import Apple, Sum, Anagram 
 
 class AppleTestCases(unittest.TestCase):
 
@@ -11,46 +11,29 @@ class SumTestCases(unittest.TestCase):
 
     def test_sum(self):
         test_list_of_numbers = Sum()
-        self.assertEquals(test_list_of_numbers.sum([1, 2, 3]), 6)
+        self.assertEqual(test_list_of_numbers.sum([1, 2, 3]), 6)
 
     def test_sum_if_empty(self):
         test_list_of_numbers = Sum()
-        self.assertEquals(test_list_of_numbers.sum([]), 0)
+        self.assertEqual(test_list_of_numbers.sum([]), 0)
 
     def test_if_one_element(self):
         test_list_of_numbers = Sum()
-        self.assertEquals(test_list_of_numbers.sum([1]), 1)
+        self.assertEqual(test_list_of_numbers.sum([1]), 1)
 
     def test_if_multiple_element(self):
         test_list_of_numbers = Sum()
-        self.assertEquals(test_list_of_numbers.sum([1, 2, 3, 4, 5]), 15)
+        self.assertEqual(test_list_of_numbers.sum([1, 2, 3, 4, 5]), 15)
 
     def test_if_null(self):
         test_list_of_numbers = Sum()
-        self.assertEquals(test_list_of_numbers.sum(None), None)       
+        self.assertEqual(test_list_of_numbers.sum(None), None)       
 
 class AnagramTestCase(unittest.TestCase):
 
     def test_anagram(self):
         test_anagram = Anagram()
         self.assertTrue(test_anagram)
-
-class CountLettersTestCase(unittest.TestCase):
-
-    def test_empty(self):
-        self.assertEqual(letter_counter(""), {})
-        
-    def test_one_letter(self):
-        self.assertEqual(letter_counter("a"), {"a": 1})
-        self.assertEqual(letter_counter("b"), {"b": 1})
-
-    def test_two_different_letters(self):
-        self.assertEqual(letter_counter("ab"), {"a": 1, "b": 1})
-
-    def test_two_same_letters(self):
-        self.assertEqual(letter_counter("aa"), {"a": 2})
-
-
 
 
 if __name__ == '__main__':
