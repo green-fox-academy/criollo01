10986b52349dcadbadbf7328b86a61071a46bc06
Criollo01 <lucanagy@protonmail.com>
2017-09-27 15:30:45 +0200

    feat(luca_nagy_test): count letters class tested

--- week-04/day-03/luca_nagy_test.py
+++ week-04/day-03/luca_nagy_test.py
@@ -35,6 +35,23 @@ class AnagramTestCase(unittest.TestCase):
         test_anagram = Anagram()
         self.assertTrue(test_anagram)
 
+class CountLettersTestCase(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(letter_counter(""), {})
+        
+    def test_one_letter(self):
+        self.assertEqual(letter_counter("a"), {"a": 1})
+        self.assertEqual(letter_counter("b"), {"b": 1})
+
+    def test_two_different_letters(self):
+        self.assertEqual(letter_counter("ab"), {"a": 1, "b": 1})
+
+    def test_two_same_letters(self):
+        self.assertEqual(letter_counter("aa"), {"a": 2})
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
