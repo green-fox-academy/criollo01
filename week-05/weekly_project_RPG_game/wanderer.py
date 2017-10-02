8977175ffc5c96baba702bcc1ea35ca97208c7ec
Criollo01 <lucanagy@protonmail.com>
2017-10-02 17:33:13 +0200

    feat(rpg): hero is moving!

--- week-05/weekly_project_RPG_game/wanderer.py
+++ week-05/weekly_project_RPG_game/wanderer.py
@@ -22,21 +22,33 @@ class Tile():
                     canvas.create_image(x*72, y*72, anchor = NW, image = self.wall)
 
 
-class Hero(Entity):
+class Hero():
 
     def __init__(self):
         self.hero = PhotoImage(file = 'assets/hero-down.png')
         self.draw_hero()
 
     def draw_hero(self):
-        canvas.create_image(0, 0, anchor = NW, image = self.hero)
-
-
+        self.entity_img = canvas.create_image(0, 0, anchor = NW, image = self.hero)
+    
 class Entity():
-
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        pass
+
+    def move(self, dx, dy):
+        canvas.move(entity.entity_img, dx, dy )
+
+entities = Entity()
+
+def moving(e):
+    if (e.keysym == 'Up'):
+        entities.move(0,-72)
+    elif (e.keysym == 'Down'):
+        entities.move(0,72)
+    elif (e.keysym == 'Right'):
+        entities.move(72,0)            
+    elif (e.keysym == 'Left'):
+        entities.move(-72,0)
 
 class View():
     pass
@@ -48,6 +60,6 @@ canvas.pack()
 
 map_draw = Tile()
 entity = Hero()
-
+root.bind("<KeyPress>", moving)
 canvas.focus_set()
 root.mainloop()
