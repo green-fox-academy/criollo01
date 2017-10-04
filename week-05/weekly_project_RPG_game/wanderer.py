d6c9244ef2b242f7ee28dfa96570c3d1bcb6f256
Criollo01 <lucanagy@protonmail.com>
2017-10-04 11:40:03 +0200

    feat(rpg): hero is moving and turning in the direction of the movement

--- week-05/weekly_project_RPG_game/wanderer.py
+++ week-05/weekly_project_RPG_game/wanderer.py
@@ -11,9 +11,9 @@ class Tile():
     def __init__(self):
         self.floor = PhotoImage(file = 'assets/floor.png')
         self.wall = PhotoImage(file = 'assets/wall.png')
-        self.draw_map(map_drawer.tiles)
+        self.drawer(map_drawer.tiles)
 
-    def draw_map(self):
+    def drawer(self, map):
         for y in range(len(map_drawer.tiles)):
             for x in range(len(map_drawer.tiles)):
                 if map_drawer.tiles[y][x] == 0:
@@ -23,74 +23,48 @@ class Tile():
 
 
 class Entity():
-
     def __init__(self):
-        self.image_boss = PhotoImage(file = 'assets/boss.png')
-        self.image_skeleton = PhotoImage(file = 'assets/skeleton.png')
-        self.x = random.randint(0, 9)
-        self.y = random.randint(0, 9)
-        self.id = 0
-
-    def draw_skeleton(self, canvas):
-        canvas.delete(self.id)
-        self.id = canvas.create_image(self.x * 72, self.y * 72, anchor = NW, image = self.image_skeleton)
-
-    def draw_boss(self, canvas):
-        canvas.delete(self.id)
-        self.id = canvas.create_image(self.x * 72, self.y * 72, anchor = NW, image = self.image_boss)
-
-
-class Hero():
-
-    def __init__(self):
-        self.hero = PhotoImage(file = 'assets/hero-down.png')
+        self.hero_down = PhotoImage(file = 'assets/hero-down.png')
+        self.hero_up = PhotoImage(file = 'assets/hero-up.png')
         self.hero_right = PhotoImage(file = 'assets/hero-right.png')
         self.hero_left = PhotoImage(file = 'assets/hero-left.png')
-        self.hero_up = PhotoImage(file = 'assets/hero-up.png')
-        self.x = 0
-        self.y = 0
-        self.id = 0
+        self.draw_hero()
 
-    def draw_hero(self, canvas, hero_image):
-        canvas.delete(self.id)
-        self.id = canvas.create_image(self.x * 72, self.y * 72, anchor = NW, image = hero_image)
+    def move(self, dx, dy):
+        canvas.move(hero.entity_img, dx, dy )
+
+    def update_costume(self, costume):
+        self.costume = costume
+        canvas.itemconfigure(self.entity_img, image=self.costume)
+
+    def draw_hero(self):
+        self.entity_img = canvas.create_image(0, 0, anchor = NW, image = self.hero_down)
+
+    def moving(self, e):
+        if (e.keysym == 'Up'):
+            self.move(0,-72)
+            self.update_costume(self.hero_up)
+        elif (e.keysym == 'Down'):
+            self.move(0,72)
+            self.update_costume(self.hero_down)
+        elif (e.keysym == 'Right'):
+            self.move(72,0)
+            self.update_costume(self.hero_right)
+        elif (e.keysym == 'Left'):
+            self.move(-72,0)
+            self.update_costume(self.hero_left)
 
 
 class View():
     pass
 
 class GameLogic():
-    def __init__(self):
-        root.bind("<KeyPress>", self.on_key_press)
-        self.floor = Tile()
-        self.hero = Hero()
-        self.floor.draw_map()
-        self.hero.draw_hero(canvas, self.hero.hero_down)
+    pass
 
-    def is_it_free_for_hero(self, x, y):
-        if 0 <= x <= 9 and 0 <= y <= 9:
-            if self.floor.tiles[y][x] == 0:
-                self.hero.draw_hero(canvas, self.hero.hero_down)
-                self.hero.x = x
-                self.hero.y = y
-
-    def on_key_press(self, e):
-        self.e = e 
-        
-        if self.e.keycode == 37:
-            self.move_hero(self.hero.x - 1, self.hero.y)
-            self.hero.draw_hero(canvas, self.hero.hero_left)
-        elif self.e.keycode == 38:
-            self.move_hero(self.hero.x, self.hero.y - 1)
-            self.hero.draw_hero(canvas, self.hero.hero_up)
-        elif self.e.keycode == 39:
-            self.move_hero(self.hero.x + 1, self.hero.y)
-            self.hero.draw_hero(canvas, self.hero.hero_right)
-        elif self.e.keycode == 40:
-            self.move_hero(self.hero.x, self.hero.y + 1)
-            self.hero.draw_hero(canvas, self.hero.image_hero_down)
-
-game = GameLogic()
 canvas.pack()
+
+map_draw = Tile()
+hero = Entity()
+root.bind("<KeyPress>", hero.moving)
 canvas.focus_set()
 root.mainloop()
