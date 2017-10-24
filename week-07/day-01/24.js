145007aaa94bdfa7e210c016146d4c8555cb697e
Criollo01 <lucanagy@protonmail.com>
2017-10-24 15:17:33 +0200

    feat(24): it's working

---
+++ week-07/day-01/24.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+for (let i = 1; i <= 501; i += 1) {
+    if (i % 2 === 0) {
+        console.log(i)        
+    }
+}
