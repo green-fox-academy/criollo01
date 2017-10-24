eb76009a9d46f19fb7c3a9b6f037f06ac9833eaf
Criollo01 <lucanagy@protonmail.com>
2017-10-24 15:24:55 +0200

    feat(25): it's working

--- week-07/day-01/15.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Create a program that writes this line 100 times:
-// "I won't cheat on the exam!"
-
-for (let i = 1; i <= 100; i += 1) {
-    console.log("I won't cheat on the exam!")
-}
