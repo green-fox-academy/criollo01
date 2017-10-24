b7bce5a101efe29adc91c46471803924a24ca33f
Criollo01 <lucanagy@protonmail.com>
2017-10-24 12:05:48 +0200

    feat(10): it's working

---
+++ week-07/day-01/10.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myName = 'Luca';
+console.log(myName);
+let myAge = 24;
+console.log(myAge);
+let myHeight = 1.64;
+console.log(myHeight);
+let isMarried = false;
+console.log(isMarried);
