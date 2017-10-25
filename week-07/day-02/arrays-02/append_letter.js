1d4a5eb243130ab526075dca018cbbfce52fb384
Criollo01 <lucanagy@protonmail.com>
2017-10-25 15:24:12 +0200

    feat(append_letter): it's working

---
+++ week-07/day-02/arrays-02/append_letter.js
@@ -0,0 +1,10 @@
+'use strict';
+// Add "a" to every string in far
+
+var far = ["kuty", "macsk", "kacs", "rók", "halacsk"];
+
+for(var i = 0; i < far.length; i++){
+    far[i] = far[i] + 'a';
+}
+
+console.log(far);
