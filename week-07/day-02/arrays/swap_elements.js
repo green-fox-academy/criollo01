bf691991f085bbd46778124d0528a7b7b5a4478d
Criollo01 <lucanagy@protonmail.com>
2017-10-25 14:14:34 +0200

    feat(swap_elements): add more simple solution

--- week-07/day-02/arrays/swap_elements.js
+++ week-07/day-02/arrays/swap_elements.js
@@ -3,10 +3,12 @@
 // - Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
 // - Swap the first and the third element of `abc`
 
-let abc = ["Arthur", "Boe", "Chloe"]
+let abc = ["Arthur", "Boe", "Chloe"];
 
 let temp = abc[0];
 abc[0] = abc[2];
 abc[2] = temp;
+
+[abc[0], abc[2]] = [abc[2], abc[0]];
 
 console.log(abc)
