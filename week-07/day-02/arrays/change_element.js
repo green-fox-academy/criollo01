c1edad6a57e7fd50584be299a467da9454182017
Criollo01 <lucanagy@protonmail.com>
2017-10-25 12:46:37 +0200

    feat(change_element): it's working

---
+++ week-07/day-02/arrays/change_element.js
@@ -0,0 +1,16 @@
+'use strict';
+// - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4 with the `.map` method 
+// - Print the fourth element as a test
+
+let s = [1, 2, 3, 8, 5, 6];
+
+let sf = s.map(function(e) {
+    if (e === 8) {
+        return e = 4;
+    } else {
+        return e
+    }
+})
+
+console.log(sf)
