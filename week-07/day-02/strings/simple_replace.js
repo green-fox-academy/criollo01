6f4cf737647f18c994c5f48939a35d7685aaa61b
Criollo01 <lucanagy@protonmail.com>
2017-10-25 14:47:01 +0200

    feat(simple_replace): it's working

---
+++ week-07/day-02/strings/simple_replace.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let example = 'In a dishwasher far far away';
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+let newExample = example.replace('dishwasher', 'galaxy');
+
+console.log(newExample)
+
