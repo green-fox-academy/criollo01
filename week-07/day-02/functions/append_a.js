18bae5af5949554477791c3369085f6fcf570767
Criollo01 <lucanagy@protonmail.com>
2017-10-25 11:49:54 +0200

    feat(append_a): it's wotking

---
+++ week-07/day-02/functions/append_a.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create a variable named `am` and assign the value `kuty` to it
+// - Write a function called `appendA` that gets a string as an input
+//   and appends an 'a' character to its end
+// - Print the result of `appendA(am)` to the console
+
+let am = 'kuty'
+
+function appendA (string) {
+    string += 'a';
+    return string;
+}
+
+console.log(appendA(am))
