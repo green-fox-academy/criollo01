e68cb1de30d8584aea65885d7657691a31dd1f6a
Criollo01 <lucanagy@protonmail.com>
2017-10-30 16:26:06 +0100

    feat(animal_farm): got totally mixed up, no idea what i'm doing

--- week-08/day-01/animal_farm.js
+++ week-08/day-01/animal_farm.js
@@ -27,26 +27,31 @@ class Farm {
     }
 
     breed() {
-        if (this.slot > 0) {
-            let animal = new Animal();
-            this.animals.push(animal);
-        } else {
-            console.log('Sorry, no more place for any other animal');
+        this.animals.length < this.slots) {
+            this.animals.push(new Animal());
         }
     }
-
     slaughter() {
-        this.animals.sort(function(a, b) {
-            return b.hunger - a.hunger;
+        animalHunger = [];
+        this.animals.forEach(function(e)) {
+            animalHunger.push(e.hunger);
         });
-        this.animals.splice(this.animals.length, 1);
+        this.animals.splice(animalHunger.indexOf(Math.min(...animalHunger)), 1);
+        this.slots++;
     }
 
     progress() {
-        
+
     }
 
     farmState() {
-        console.log('The farm has ' + this.animals.length + ' living animals, we are bankrupt.');
+        let stateOfFarm = '';
+        if (this.animals.length === 0) {
+            stateOfFarm = 'bankrupt';
+        } else if (this.animals.length < this.slot) {
+            stateOfFarm = 'okay';
+        } else {
+            stateOfFarm = 'full';
+        }
+        console.log('The farm has ' + this.animals.length + ' living animals, we are ' + stateOfFarm.');
     }
-}
