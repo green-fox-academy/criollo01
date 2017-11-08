fa9f6d1bbf02024cc76c4c1dec0c8f24630a6b1a
Criollo01 <lucanagy@protonmail.com>
2017-11-08 15:20:29 +0100

    feat(doubling): it's working

--- week-09/day-03/app.js
+++ week-09/day-03/app.js
@@ -7,4 +7,14 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.get('/doubling', function(req, res){
+    if (req.query.input) {
+        res.json({'received' : req.query.input, 'result' : req.query.input*2});
+    } else {
+        res.json({'error' : "Please provide an input!"});       
+    }
+});
+
+
+
+app.listen(8080);
