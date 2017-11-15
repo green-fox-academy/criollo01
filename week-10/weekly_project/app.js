c5d257c334846ba94c7875209dad5b4d46b3cf17
Criollo01 <lucanagy@protonmail.com>
2017-11-15 14:32:30 +0100

    feat(backend): listing two placeholder lists, server running

---
+++ week-10/weekly_project/app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + 'index.html'));
+app.use(express.json());
+
+app.get('/playlists', function(req, res) {
+    res.json([
+        { "id": 1, "title": "Favorites", "system": 1},
+        { "id": 2, "title": "Music for programming", "system": 0},
+        { "id": 3, "title": "Driving", "system": 0},
+        { "id": 5, "title": "Fox house", "system": 0},
+    ])
+})
+
+app.get('/playlist-tracks', function(req, res) {
+    res.json([
+        {
+            'id': 2, 
+            'title': 'Sea of Dreams', 
+            'artist': 'Oberhofer', 
+            'duration': 4.12, 
+            'path': '/assets/Oberhofer-Sea_of_Dreams.mp3',
+        },
+        {
+            'id': 3,
+            'title': 'Brennum allt',
+            'artist': 'Úlfur Úlfur',
+            'duration': 4.05,
+            'path': '/assets/ulfur_ulfur.mp3',
+        },
+    ])
+})
+
+app.listen(8080, function() {
+    console.log('The server is running.')
+})
