66dcc90a89c7bb7a9fa3a9e5bd2d00808f8aa810
Criollo01 <lucanagy@protonmail.com>
2017-11-15 16:08:18 +0100

    feat(backend): minor changes

--- week-10/weekly_project/app.js
+++ week-10/weekly_project/app.js
@@ -3,10 +3,9 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.use('/assets', express.static('./assets'));
-
-app.get('/', function (req, res){
-    res.sendFile(__dirname + '/foxplayer.html')
+app.use(express.static( __dirname + '/assets'));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/playlists', function(req, res) {
