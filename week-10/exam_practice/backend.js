46fe61480663a5b2af52b8377b6e8127ffb26c52
Criollo01 <lucanagy@protonmail.com>
2017-11-16 17:19:24 +0100

    feat(backend): app get all book data

--- week-10/exam_practice/backend.js
+++ week-10/exam_practice/backend.js
@@ -18,6 +18,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
 
+var books = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id ';
+
 app.get('/allbooks', function(req, res) {
     var allBooksTitle = [];
     connection.query('SELECT book_name FROM book_mast', function(err, result, request) {
@@ -26,6 +28,16 @@ app.get('/allbooks', function(req, res) {
         });
     res.send(allBooksTitle);
     })
+})
+
+app.get('/allbookdata', function(req, res) {
+    connection.query(books, function(err, data) {
+        if (err) {
+            console.log(err.toString);
+        }
+        res.send({'books': data});
+    })
+    console.log('Data received from database');
 })
 
 app.listen(3000, () => console.log("server running"));
