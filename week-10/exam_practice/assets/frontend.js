fed147709de7925f95bd1126bc6f800e5b3e463a
Criollo01 <lucanagy@protonmail.com>
2017-11-16 17:19:52 +0100

    feat(frontend): print all book data to a table

--- week-10/exam_practice/assets/frontend.js
+++ week-10/exam_practice/assets/frontend.js
@@ -1,13 +1,15 @@
 'use strict'
 
 const url = "http://localhost:3000";
+let tableElement = document.querySelector('table');
+
 
 function ajax (method, url, callback) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.onload = function() {
     console.log(xhr.responseText);
-      callback(JSON.parse(xhr.responseText));
+      callback(xhr.responseText);
     };
     xhr.send();
   };
@@ -18,10 +20,23 @@ function appendToBody(element) {
     document.body.appendChild(someElement);
 }
 
-function listBookTitles(result){
-    result.forEach(element => {
+function listBookTitles(response) {
+    response.forEach(element => {
         appendToBody(element);
     });
 }
 
-ajax('GET', url + '/allbooks', listBookTitles)
+function listAllData(response) {
+    let information = JSON.parse(response);
+    information.books.forEach(element => {
+        let tableData = '<tr><td>' + element.book_name + 
+                        '</td><td>' + element.aut_name + 
+                        '</td><td>' + element.cate_descrip + 
+                        '</td><td>' + element.pub_name + 
+                        '</td><td>' + element.book_price + 
+                        '</td></tr>';
+        tableElement.innerHTML += tableData;
+    });
+}
+
+ajax('GET', url + '/allbookdata', listAllData)
