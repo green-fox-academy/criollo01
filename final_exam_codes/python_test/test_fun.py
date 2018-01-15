7dc6276b8e016fa8892fe391898258575d79dd20
Criollo01 <lucanagy@protonmail.com>
2018-01-15 10:12:36 +0100

    feat(test_python): add failing test example

--- final_exam_codes/python_test/test_fun.py
+++ final_exam_codes/python_test/test_fun.py
@@ -4,6 +4,11 @@ from fun import fun
 class TestFun(unittest.TestCase):
     def test_fun(self):
         self.assertEqual(fun(3), 4)
+    
+
+    def test_fun_failing(self):
+        self.assertEqual(fun(3), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
